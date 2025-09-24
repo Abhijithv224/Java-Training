095a8e53b8243cb246db7a952466d851199c2471
Abhijithv224 <abhijithv2204@gmail.com>
2025-09-24 21:54:44 +0530

    Add files via upload

---
+++ problem13.java
@@ -0,0 +1,53 @@
+/*13.Write a program that reads a number from 1 to 12 and prints the corresponding month name and the
+number of days in the month using a switch statement.*/
+import java.io.*;
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        
+        int month = scan.nextInt();
+
+        switch (month) {
+            case 1:
+                System.out.println("January and  31 days");
+                break;
+            case 2:
+                System.out.println("February and 28 days");
+                break;
+            case 3:
+                System.out.println("March and 31 days");
+                break;
+            case 4:
+                System.out.println("April and 30 days");
+                break;
+            case 5:
+                System.out.println("May and 31 days");
+                break;
+            case 6:
+                System.out.println("June and 30 days");
+                break;
+            case 7:
+                System.out.println("July and 31 days");
+                break;
+            case 8:
+                System.out.println("August and 31 days");
+                break;
+            case 9:
+                System.out.println("September and 30 days");
+                break;
+            case 10:
+                System.out.println("October and 31 days");
+                break;
+            case 11:
+                System.out.println("November and 30 days");
+                break;
+            case 12:
+                System.out.println("December and 31 days");
+                break;
+            default:
+                System.out.println("incorrect number");
+                break;
+        }
+	}
+}
